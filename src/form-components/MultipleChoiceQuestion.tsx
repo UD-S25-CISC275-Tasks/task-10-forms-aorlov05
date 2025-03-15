bb088038ac45e3c66a41b6a9ca61c884075d921a
aorlov05 <aorlov@udel.edu>
2025-03-15 16:22:44 -0400

    Implement multiple choice question

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [givenAnswer, setGivenAnswer] = useState<string>(options[0]);
+
+    function updateGivenAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setGivenAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="answers">
+                <Form.Label>Pick an answer: </Form.Label>
+                <Form.Select value={givenAnswer} onChange={updateGivenAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{givenAnswer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
